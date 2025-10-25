f1b9d7ee9bddc4c4e7de6ec53b3846d3116cad26
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-10-25 23:47:41 +0600

    Initial commit

---
+++ Day 2/day2_3.c
@@ -0,0 +1,18 @@
+// C program to count the Lines of the input
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(){
+    char s[1000];
+    scanf("%[^$]", s);
+    int count=1;
+
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='\n'){
+            count++;
+        }
+    }
+    printf("Total Lines: %d\n", count);
+    return 0;
+}
