2a3cb4e263b8df25e715a0418a471f19171e70a3
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-10-25 22:11:45 +0600

    Create day2_1.c

---
+++ Day 2/day2_1.c
@@ -0,0 +1,29 @@
+// C program to count Vowel, Consonant & Digit 
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(){
+    char str[1000];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    int vowels=0, consonants=0, digits=0;
+
+    for(int i=0;str[i]!='\0';i++){
+        char ch = tolower(str[i]);
+        if(isdigit(ch)){
+            digits++;
+        }
+        else if(ch>='a' && ch<='z'){
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+    printf("Total Vowels: %d\n", vowels);
+    printf("Total Consonants: %d\n", consonants);
+    printf("Total Digits: %d\n", digits);
+    return 0;
+}
