d82954c99f91b6665159fc335cf8e0437c65b8eb
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-11-22 15:59:21 +0600

    Fix formatting and improve identifier validation

--- Day 3/day3_4.c
+++ Day 3/day3_4.c
@@ -1,23 +1,23 @@
 // C program to recognize set of identifiers valid or not
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
 #include<ctype.h>
 
-int main(){
+int main() {
     char s[1000];
     printf("Enter an identifier: ");
     fgets(s, sizeof(s), stdin);
-    s[strcspn(s, "\n")] = '\0';
-    int id=1;
+    int id = 1;
 
-    // check first character: must be letter or underscore  
-    if(!isalpha(s[0]) && s[0]!='_'){
-        id=0;
+    s[strcspn(s, "\n")] = '\0';
+
+    if(!(isalpha(s[0]) || s[0]=='_')){
+        id = 0;
     }
-    // check remaining characters: must be letter, digit, or underscore
+
     for(int i=1;i<strlen(s);i++){
         if(!(isalnum(s[i]) || s[i]=='_')){
-            id=0;
+            id = 0;
             break;
         }
     }
@@ -26,4 +26,5 @@ int main(){
     } else {
         printf("Invalid Identifier\n");
     }
+    return 0;
 }
